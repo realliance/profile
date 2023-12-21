049f59811c85b52a672cc7b421665d8e81e5a353
Christopher Miller <hello@chrismiller.xyz>
2023-12-21 15:07:29 -0800

    good enough to deploy for now

--- frontend/src/pages/GroupList.tsx
+++ frontend/src/pages/GroupList.tsx
@@ -1,23 +1,20 @@
-import { useLoaderData } from "react-router-dom";
-import { Group } from "../util/group";
-import { useContext, useEffect, useMemo } from "react";
-import { AuthContext } from "../contexts/AuthContext";
-import { createGroup } from "../util/api";
+import { useLoaderData } from 'react-router-dom';
+import { Group } from '../util/group';
+import { useMemo } from 'react';
 
 export function GroupList() {
-  const { token } = useContext(AuthContext);
+  //const { token } = useContext(AuthContext);
   const groups = useLoaderData() as Group[];
 
-  const groupContent = useMemo(() => groups.map((group) => (
-    <div>
-      {group.name}
-    </div>
-  )), [groups]);
+  const groupContent = useMemo(
+    () => groups.map((group) => <div>{group.name}</div>),
+    [groups],
+  );
 
   return (
     <div className="container mx-auto flex flex-col gap-3">
       <h1 className="text-4xl font-bold">Available Groups</h1>
-      {groupContent}
+      {groupContent.length > 0 ? groupContent : <p>No groups available</p>}
     </div>
-  )
+  );
 }
