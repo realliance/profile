2d3739ed44048aac5197fe9290b55fc40cb77ea1
Chris Miller <chrmiller@microsoft.com>
2024-01-01 16:29:16 -0500

    auto gen id

--- api/migrations/1703100640956-CreateGroup.ts
+++ api/migrations/1703100640956-CreateGroup.ts
@@ -1,49 +1,49 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm"
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateGroup1703100640956 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'group',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+        ],
+      }),
+      true,
+    );
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(
-          new Table({
-            name: 'group',
-            columns: [
-              {
-                name: 'id',
-                type: 'int',
-                isPrimary: true,
-              },
-              {
-                name: 'name',
-                type: 'varchar',
-              },
-            ],
-          }),
-          true,
-        );
+    await queryRunner.createTable(
+      new Table({
+        name: 'user_groups_group',
+        columns: [
+          {
+            name: 'userId',
+            type: 'int',
+            isPrimary: true,
+          },
+          {
+            name: 'groupId',
+            type: 'int',
+            isPrimary: true,
+          },
+        ],
+      }),
+      true,
+    );
+  }
 
-        await queryRunner.createTable(
-            new Table({
-              name: 'user_groups_group',
-              columns: [
-                {
-                  name: 'userId',
-                  type: 'int',
-                  isPrimary: true,
-                },
-                {
-                  name: 'groupId',
-                  type: 'int',
-                  isPrimary: true,
-                },
-              ],
-            }),
-            true,
-          );
-      }
-    
-      public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('group');
-        await queryRunner.dropTable('user_groups_group');
-      }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('group');
+    await queryRunner.dropTable('user_groups_group');
+  }
 }
